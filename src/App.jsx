0b1a98c5a0abfc959fade314141cffa09cc4633d
Humaira Islam <ihumaira.webfront@gmail.com>
2024-06-10 23:41:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { FaCirclePlus } from "react-icons/fa6"
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="main">
+      <FaCirclePlus />
+      <h1 className='text-2xl font-bold'>1</h1>
+
+      </div>
+      
+      
+    </>
+  )
+}
+
+export default App
